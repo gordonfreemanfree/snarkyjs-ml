a2fe82dac37dec15549b9bb37a1e5505f0823722
gordonfreemanfree <haas.benedikt.haas@gmail.com>
2022-12-31 18:39:01 +0100

    Deploy gh-pages

---
+++ ui/out/_next/static/tnZ_wYhQdrbdO1oh1iRQi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-f4bfc7bf19159b71.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-75f8c62d1b846d4f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
